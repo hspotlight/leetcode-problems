30d3377e29b09b45e4fb09c1209c7f6995b8dd9d
Pakawat Anekwiroj <pakawat.ane@email.com>
2021-02-13 19:11:47 +0700

    l1540 - Add logic to handle quotient case

--- src/l1540-can-convert-string-in-k-move/convertString2.test.ts
+++ src/l1540-can-convert-string-in-k-move/convertString2.test.ts
@@ -28,6 +28,16 @@ describe('convertString2', () => {
             // only one change
             ['ab', 'aa', 2, false],
             ['ab', 'aa', 25, true],
+            // diff change under k
+            ['ab', 'd', 2, false],
+            ['ab', 'bd', 2, true],
+            ['ab', 'bc', 2, false],
+            ['ab', 'bc', 27, true],
+            ['abc', 'bcd', 53, true],
+            ['input', 'ouput', 9, true],
+            ['abc', 'bcd', 10, false],
+            ['mpzzwh', 'kaeblv', 24, true],
+            ['qsxkjbfz', 'xyfirptk', 73, true],
         ].forEach(input => {
             const [s, t, k, expectedResult] = input
             it(`should return ${expectedResult} when given ${s} ${t} ${k}`, () => {
--- src/l1540-can-convert-string-in-k-move/convertString2.ts
+++ src/l1540-can-convert-string-in-k-move/convertString2.ts
@@ -1,17 +1,49 @@
 const nCharacters = 26;
 
 const canConvertString2 = (s: string, t: string, k: number): boolean => {
-    
-    let result = [...s].map((_, i: number) => {
-        return countRotate(s[i], t[i]) <= k;
-    }).reduce((isConvertable, is) => isConvertable && is, true)
+    if (s.length !== t.length) return false
 
+    let counts = [...s].map((_, i: number) => {
+        return countRotate(s[i], t[i]);
+    }).filter(n => n !== 0)
+
+    const freq = getFreq2(counts)
+
+    const [quotient, remainder] = getQuotientAndRemainder2(k, nCharacters);
+
+    let result = true;
+    Object.keys(freq).forEach(key => {
+        const count = +key
+        const freqValue = freq[key]
+        const quotientCase = freqValue <= quotient
+        const remainderCase = (freqValue === quotient + 1 && count <= remainder);
+        result = result && (quotientCase || remainderCase)
+    })
     return result
 }
 
 module.exports = {
     canConvertString2
 };
+
+const getFreq2 = (numbers: number[]) => {
+    const freq = Object.assign({})
+
+    numbers.forEach(number => {
+        if (!freq[number]) {
+            freq[number] = 0;
+        }
+        freq[number]++;
+    })
+
+    return freq;
+}
+
+const getQuotientAndRemainder2 = (dividend: number, divisor: number): [number, number] => {
+    const quotient = Math.floor(dividend / divisor);
+    const remainder = dividend % divisor;
+    return [quotient, remainder]
+}
 
 function countRotate(s: string, t: string): number {
     let count;
