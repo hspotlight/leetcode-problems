23a365bdc05893cbaa04a297d58a8b9a05683b53
Pakawat Anekwiroj <pakawat.ane@email.com>
2021-02-21 13:46:36 +0700

    l207 - Refactor

--- src/l207-Course-Schedule/canFinish.ts
+++ src/l207-Course-Schedule/canFinish.ts
@@ -4,40 +4,50 @@ export function canFinish(numCourses: number, prerequisites: number[][]): boolea
 };
 
 export function enterCourse(numCourses: number, prerequisites: number[][]): number {
-    let count = 0
-    const depCount: number[] = [];
-    const done: boolean[] = [];
-    const depMap = Object.assign({})
-    for (let i = 0; i < numCourses; i++) {
-        depCount[i] = 0
-        depMap[i] = []
-        done[i] = false
-    }
-    for (let i = 0; i < prerequisites.length; i++) {
-        const [course, dep] = prerequisites[i]
-        depMap[dep].push(course);
-        depCount[course]++;
-    }
+    const courseDep = getCourseDep(numCourses, prerequisites);
 
-    const doableTasks = []
-    for (let i = 0; i < numCourses; i++) {
-        if (depCount[i] === 0) {
-            doableTasks.push(i);
-        }
-    }
+    let count = 0
+    const done: boolean[] = [];
+    done.length = numCourses
+    done.fill(false)
+
+    const doableTasks = courseDep.map((course, index) => {
+        return course.count === 0 ? index : -1
+    }).filter(id => id !== -1)
 
     while (doableTasks.length > 0) {
         const task = doableTasks.shift() ?? -1;
         if (!done[task]) {
             done[task] = true;
             count++;
-            depMap[task].forEach((newTask: number) => {
-                depCount[newTask]--;
-                if (depCount[newTask] === 0) {
+            courseDep[task].dependedBy.forEach((newTask: number) => {
+                courseDep[newTask].count--;
+                if (courseDep[newTask].count === 0) {
                     doableTasks.push(newTask)
                 }
             })
         }
     }
     return count;
+}
+
+type CourseDep = {
+    count: number;
+    dependedBy: number[];
+};
+
+function getCourseDep(numCourses: number, prerequisites: number[][]) {
+    const courseDep: CourseDep[] = []
+    for (let i = 0; i < numCourses; i++) {
+        courseDep[i] = {
+            count: 0,
+            dependedBy: []
+        }
+    }
+    for (let i = 0; i < prerequisites.length; i++) {
+        const [course, dependency] = prerequisites[i];
+        courseDep[dependency].dependedBy.push(course);
+        courseDep[course].count++;
+    }
+    return courseDep
 }
